af6a1125ce78c42c8df3e820de3231f0966487fc
Amine Oudija <aoudija@e2r13p4.1337.ma>
2023-12-30 09:57:01 +0100

    fixed header server

--- server.hpp
+++ server.hpp
@@ -36,6 +36,37 @@ class server{
 	string	port, serverName, request, response, ip;
 	bool is_default;
 	int my_default, _s_listener, _s_connection;
+
+	// ayoub ---------
+	string				root;
+	string				index;
+	int					client_body_limit;
+	bool				autoindex;
+	vector<string>		allow_methods;
+	map<int,string>		error_page;
+	vector<Location>	locations;
+
+	//===============
+	void	checkfirstline(std::string str, int line);
+	void	checklastline(std::string str, int line, int firstline);
+	void	seter(std::string str, int line);
+	void	set_value(std::vector<std::string> list, int token, int line);
+	void	Myhostport(std::vector<std::string> list, int line);
+	std::vector<std::string> splitString(const std::string& input, const std::string& delm) ;
+	std::string throwmessage(int number, const std::string& str);
+	std::string withoutsemicolon(std::string str);
+	bool 	isInMyList(const std::string& target, int &token);
+	bool 	isWhitespace(const std::string& str);
+	void	Myserver_name(std::vector<std::string> list, int line);
+	void	Myroot(std::vector<std::string> list, int line);
+	void	Myindex(std::vector<std::string> list, int line);
+	void	Myclient_body_limit(std::vector<std::string> list, int line);
+	void	Myautoindex(std::vector<std::string> list, int line);
+	void	Myallow_methods(std::vector<std::string> list, int line);
+	void	Myerror_page(std::vector<std::string> list, int line);
+	void	Mylocations(std::vector<Location>&);
+	void	setmylocation(std::map<int, std::string>::const_iterator &it, std::map<int, std::string> &server);
+	//================
 	public:
 		void	portSetter(string prt);
 		void	set_my_default(int index);
@@ -53,6 +84,30 @@ class server{
 		int		get_my_default();
 		int		get_slistener();
 		int		get_sconncetion();
+	
+		//===========================
+		void	setPort(std::string);
+		void	setIp(std::string);
+		void	setServerName(std::string);
+		void	setRoot(std::string);
+		void	setIndex(std::string);
+		void	setClientBodyLimit(int);
+		void	setAutoindex(bool);
+		void	setAllowMethods(std::string);
+		void	setErrorPage(int, std::string);
+		void	setLocations(Location&);
+
+		std::string	getServerName(void) const;
+		std::string	getPort(void) const;
+		std::string	getIp(void) const;
+		std::string	getRoot(void) const;
+		std::string	getIndex(void) const;
+		int			getClientBodyLimit(void) const;
+		bool		getAutoindex(void) const;
+		std::vector< std::string>		getAllowMethods(void) const;
+		std::map<int, std::string>		getErrorPage(void) const;
+		std::vector<Location>			getLocations(void) const;
+		void	parse(std::map<int, std::string>&);
 };
 
 class serversInfos
