707721c8c83078b24173b6db1fec45788daed424
zakariabouayyad <zakariabouayyad5@gmail.com>
2024-02-03 10:57:24 +0100

    fixed 501 error

--- client/Post.cpp
+++ client/Post.cpp
@@ -9,39 +9,27 @@ void	_post_(request& requestObj, server& _server){
 		requestObj.setStatusCode("201 Created");
 		requestObj.parseRequest(requestObj.theBody, _server);
 	}
-	if (!_server.getUpload())
-			cout << RED << "UPLOAD IS OFF" << RESET_TEXT << endl;
-	if (checkExistance(requestObj))
-		return ;
-	if (isDirectory(requestObj.getFilePath().c_str()))
-	{
-		if (!endsWithSlash(requestObj.getFilePath()))
+	if (!_server.getUpload()){
+		if (checkExistance(requestObj))
+			return ;
+		if (isDirectory(requestObj.getFilePath().c_str()))
 		{
-			codeNpath(requestObj,"301 Moved Permanently",
-				string(requestObj.getFilePath() + "/").c_str());
-			if (!_server.getIndex().empty()) {
-				if (!_server.getAutoindex()) {
+			if (!endsWithSlash(requestObj.getFilePath()))
+			{
+				codeNpath(requestObj,"301 Moved Permanently",
+					string(requestObj.getFilePath() + "/").c_str());
+				return ;
+			}
+			else {
+				if (!_server.getIndex().empty()) {
 					codeNpath(requestObj, "403 Forbidden", errorPageTamplate("403, Forbidden.").c_str());
 					return ;
 				}
-				else {
-					generateAutoIndex(requestObj.getFilePath(), "autoindex.html");//?need to do lmsa l file d index
-					codeNpath(requestObj,"301 Moved Permanently", "autoindex.html");
-					return ;
-				}
+				else 
+					isCGI(requestObj, requestObj.getFilePath(), _server);
 			}
-			
-			return ;
 		}
-		else {
-			if (!_server.getIndex().empty()) {
-				codeNpath(requestObj, "403 Forbidden", errorPageTamplate("403, Forbidden.").c_str());
-				return ;
-			}
-			else 
-				isCGI(requestObj, requestObj.getFilePath(), _server);
-		}
+		else 
+			isCGI(requestObj, requestObj.getFilePath(), _server);
 	}
-	else 
-		isCGI(requestObj, requestObj.getFilePath(), _server);
 }
